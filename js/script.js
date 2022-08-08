42653273252540653aafcfcbb7fcca63c44a9e10
nxe <rosiegbartlett@gmail.com>
2022-08-08 23:02:04 +0100

    footer js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const header =
+`
+
+`
+
+const footer =
+`
+<footer class="mt-auto">
+  <div class="px-lg-5 py-3">
+    <div class="row text-center">
+      <h6 class="lead">Owned by The Future Place hello</h6>
+    </div>
+  </div>
+</footer>
+`
+
+function init() {
+	document.body.innerHTML = header + document.body.innerHTML + footer
+}
