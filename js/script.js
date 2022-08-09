d596966000a19da556bcf28779d676beb32cf4c9
nxe <rosiegbartlett@gmail.com>
2022-08-09 09:37:07 +0100

    why must it be this shit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-const header =
-`
-
-`
-
-const footer =
-`
-<footer class="mt-auto">
-  <div class="px-lg-5 py-3">
-    <div class="row text-center">
-      <h6 class="lead">Owned by The Future Place hello</h6>
-    </div>
-  </div>
-</footer>
-`
+const footer = `<footer class="mt-auto"><div class="px-lg-5 py-3"><div class="row text-center"><h6 class="lead">Owned by The Future Place hello</h6></div></div></footer>`
 
 function init() {
-	document.body.innerHTML = header + document.body.innerHTML + footer
+	document.body.innerHTML = document.body.innerHTML + footer
 }
